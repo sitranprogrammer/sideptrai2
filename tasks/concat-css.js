665cd0853cc6fcc8c9ae3a5dbf21f0a4a84c2114
Phaj Nướng <baonguyenyam@gmail.com>
2018-08-12 13:56:25 +0700

    add fontawesome

--- tasks/concat-css.js
+++ tasks/concat-css.js
@@ -1,7 +1,7 @@
 module.exports = function (gulp, $, browserSync) {
 	gulp.task('concat-css', function () {
 		return gulp.src([
-				'bower_components/bootstrap/dist/css/bootstrap.min.css',
+				'bower_components/font-awesome/css/font-awesome.min.css',
 			])
 			.pipe($.concat('thuvien.css'))
 			.pipe(gulp.dest('./dist/css'));
